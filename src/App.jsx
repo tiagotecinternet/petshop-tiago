e4760012e94c8408cc027740051a672c12be200d
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-20 09:25:41 -0300

    Criação básica das páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Contato from "./pages/Contato/Contato";
 import Home from "./pages/Home/Home";
+import Produtos from "./pages/Produtos/Produtos";
+import Sobre from "./pages/Sobre/Sobre";
 
 const App = () => {
   return (
@@ -7,6 +10,9 @@ const App = () => {
       <Cabecalho />
       <main className="limitador">
         <Home />
+        <Produtos />
+        <Sobre />
+        <Contato />
       </main>
     </>
   );
