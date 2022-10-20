d30d2cae15622b0e0941ecd4e3848b00510b07e1
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-20 09:33:59 -0300

    Adição de artigos em Produtos

--- src/pages/Produtos/Produtos.jsx
+++ src/pages/Produtos/Produtos.jsx
@@ -3,6 +3,33 @@ const Produtos = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Conheça nossos produtos</h2>
+
+      <div className={estilos.produtos}>
+        <article>
+          <h3>Produto 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            dolore nemo optio blanditiis accusamus totam sint ab impedit eius.
+            Molestias.
+          </p>
+        </article>
+        <article>
+          <h3>Produto 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            dolore nemo optio blanditiis accusamus totam sint ab impedit eius.
+            Molestias.
+          </p>
+        </article>
+        <article>
+          <h3>Produto 3</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            dolore nemo optio blanditiis accusamus totam sint ab impedit eius.
+            Molestias.
+          </p>
+        </article>
+      </div>
     </section>
   );
 };
