51d3bd31940f5ed1e89c5a5c615f5f9093bf7406
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-21 09:55:49 -0300

    Adição do Caixa no Contato e 404

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,14 +1,15 @@
+import Caixa from "../../components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
 const Contato = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Fale Conosco</h2>
 
-      <div id="contato">
+      <Caixa id="contato">
         <p>
           <i>Em breve teremos um formulário com biblioteca de componentes...</i>
         </p>
-      </div>
+      </Caixa>
     </section>
   );
 };
--- src/pages/Pagina404/Pagina404.jsx
+++ src/pages/Pagina404/Pagina404.jsx
@@ -1,19 +1,20 @@
 import estilos from "./Pagina404.module.css";
 import imagem404 from "../../assets/images/404.svg";
+import Caixa from "../../components/Caixa/Caixa";
 
 const Pagina404 = () => {
   return (
     <section className={estilos.pagina404}>
       <h2 className={estilos.titulo_secao}>Au au au! Foi mal!</h2>
 
-      <div id="pagina404">
+      <Caixa id="pagina404">
         <h3>Ops! Página não encontrada!</h3>
         <img
           src={imagem404}
           className={estilos.imagem404}
           alt="Ilustração cachorrinho bunitinho"
         />
-      </div>
+      </Caixa>
     </section>
   );
 };
