5bee3b9da57c064e155344e4b9346f3870d83f44
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-24 11:50:14 -0300

    useEffect para controlar fetch api

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -1,24 +1,40 @@
+import { useState, useEffect } from "react"; // Hooks do React
+
 import serverApi from "../../api/servidor-api";
 import estilos from "./ListaPosts.module.css";
 const ListaPosts = () => {
-  console.log(serverApi);
-  const postsTemp = [];
+  /* Iniciamos o state do componente com um array vazio,
+  para posteriormente "preenchê-lo" com os dados vindos da API.
+  Esta atribuição será feita com auxílio do setPosts. */
+  const [posts, setPosts] = useState([]);
 
-  const getPosts = async () => {
-    try {
-      const resposta = await fetch(`${serverApi}/posts`);
-      const dados = await resposta.json();
-      console.log(dados);
-    } catch (error) {
-      console.log("Deu ruim! " + error.message);
+  useEffect(() => {
+    async function getPosts() {
+      try {
+        const resposta = await fetch(`${serverApi}/posts`);
+        const dados = await resposta.json();
+        setPosts(dados);
+      } catch (error) {
+        console.log("Deu ruim! " + error.message);
+      }
     }
-  };
+    getPosts();
+  }, []);
 
-  getPosts();
+  /* Sobre o useEffect
+  Este hook visa permitir um maior controle sobre "efeitos colaterais" na execução do componente.
+  
+  Recebe dois parâmetros:
+  1º: função callback com o que será executado
+  2º: lista de dependências que indicarão ao useEffect quando ele deverá funcionar
+
+  -Se não passar a lista (ou seja, se deixar sem []), useEffect executará toda vez que o componente for renderizado. Portanto, o callback se torna um loop infinito.
+
+  -Se passar a lista vazia (ou seja, deixar o [] vazio), useEffect executará somente no momento que o componente é renderizado pela primeira vez, evitando o loop infinito do callback.  */
 
   return (
     <div className={estilos.lista_posts}>
-      {postsTemp.map(({ id, titulo, subtitulo }) => (
+      {posts.map(({ id, titulo, subtitulo }) => (
         <article className={estilos.post} key={id}>
           <h3> {titulo} </h3>
           <p>{subtitulo}</p>
