c62987a72f1911a1143ea18541af967a0e9d72ac
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-26 10:03:04 -0300

    Correção no enunciado

--- src/components/ListaCategorias/ListaCategorias.jsx
+++ src/components/ListaCategorias/ListaCategorias.jsx
@@ -3,4 +3,4 @@
 1) Montar o componente (rafce)
 2) Programar a comunicação com a API para ler os dados do endpoint "categorias"
 3) Manipular os states do componente
-4) Fazer o componente ListaCategorias retornar uma lista com os nomes das categorias (<ul> <li>).  */
+4) Fazer o componente ListaCategorias retornar uma div com a classe "lista_categorias", contendo uma lista com os nomes das categorias (<ul> <li>).  */
