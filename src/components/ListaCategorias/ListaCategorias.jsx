ed2849afacaaf579c96a182fa33319a5278fe646
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-10-27 08:56:48 -0300

    Loading e ajuste CSS

--- src/components/ListaCategorias/ListaCategorias.jsx
+++ src/components/ListaCategorias/ListaCategorias.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import serverApi from "../../api/servidor-api";
+import LoadingDesenho from "../LoadingDesenho/LoadingDesenho";
 import estilos from "./ListaCategorias.module.css";
 
 const ListaCategorias = () => {
@@ -9,12 +10,14 @@ const ListaCategorias = () => {
 2º parâmetro: função responsável por atualizar (setter)
 Obs.: o que colocamos no useState representa o valor inicial */
   const [categorias, setCategorias] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getCategorias() {
       try {
         const resposta = await fetch(`${serverApi}/categorias`);
         const dados = await resposta.json();
+        setLoading(false);
 
         /* Precisamos passar os dados capturador da API
         para o state do componente via Setter (obrigatório!) */
@@ -29,17 +32,15 @@ Obs.: o que colocamos no useState representa o valor inicial */
     getCategorias();
   }, []);
 
+  if (loading) return <LoadingDesenho />;
+
   return (
     <div className={estilos.lista_categorias}>
       <ul>
-        {/* {categorias.map((categoria) => {
-          return <li key={categoria.id}> {categoria.nome} </li>;
-        })} */}
-
         {categorias.map(({ id, nome }) => {
           return (
             <li key={id}>
-              <Link to="">{nome}</Link>
+              <Link to={`/categoria/${nome}`}>{nome}</Link>
             </li>
           );
         })}
