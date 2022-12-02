7b2594300b8cd9c3eeace721c4ad748b61117b6c
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-12-02 11:42:06 -0300

    Update ListaCategorias.jsx

--- src/components/ListaCategorias/ListaCategorias.jsx
+++ src/components/ListaCategorias/ListaCategorias.jsx
@@ -5,26 +5,26 @@ import LoadingDesenho from "../LoadingDesenho/LoadingDesenho";
 import estilos from "./ListaCategorias.module.css";
 
 const ListaCategorias = () => {
-  /* Atribuição do useState para manipular os dados do componente 
-1º parâmetro: variável que terá os dados
-2º parâmetro: função responsável por atualizar (setter)
-Obs.: o que colocamos no useState representa o valor inicial */
   const [categorias, setCategorias] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getCategorias() {
       try {
-        const resposta = await fetch(`${serverApi}/categorias`);
+        const resposta = await fetch(`${serverApi}/categorias.json`);
         const dados = await resposta.json();
+        const listaDeCategorias = [];
+
+        for (const categoria in dados) {
+          const objetoCategoria = {
+            id: categoria,
+            nome: dados[categoria].nome,
+          };
+          listaDeCategorias.push(objetoCategoria);
+        }
+
         setLoading(false);
-
-        /* Precisamos passar os dados capturador da API
-        para o state do componente via Setter (obrigatório!) */
-        setCategorias(dados);
-
-        /* teste do state de categorias */
-        console.log(dados);
+        setCategorias(listaDeCategorias);
       } catch (error) {
         console.log("Deu ruim! " + error.message);
       }
